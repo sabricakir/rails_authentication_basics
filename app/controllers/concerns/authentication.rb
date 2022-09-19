78dc4ae6d78a5b9579f860b34b411d99b5a4bb0e
sabricakir <sabricakir86@gmail.com>
2022-09-19 10:00:09 +0300

    add forget and remember methods

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -24,10 +24,24 @@ module Authentication
     redirect_to login_path, alert: "You need to login to access that page." unless user_signed_in?
   end
 
+  def forget(user)
+    cookies.delete :remember_token
+    user.regenerate_remember_token
+  end
+
+  def remember(user)
+    user.regenerate_remember_token
+    cookies.permanent.encrypted[:remember_token] = user.remember_token
+  end
+
   private
 
   def current_user
-    current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+    current_user ||= if session[:current_user_id].present?
+     User.find_by(id: session[:current_user_id])
+    elsif cookies.permanent.encrypted[:remember_token].present?
+     User.find_by(remember_token: cookies.permanent.encrypted[:remember_token])
+    end
   end
 
   def user_signed_in?
